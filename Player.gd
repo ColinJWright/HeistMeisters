3713372fa8229cb02687bd8550b584c5b0864fb3
ColinJWright <colinwright2771@gmail.com>
2020-11-16 16:40:28 +0000

    Lesson 55 complete. Added a Night Vision mode.

--- Player.gd
+++ Player.gd
@@ -21,6 +21,6 @@ func update_movement():
 	else:
 		motion.x = lerp(motion.x, 0 , FRICTION)
 
-#func _input(event):
-#	if Input.is_action_just_pressed("torch_toggle"):
-#		$Torch.enabled = !$Torch.enabled
+func _input(event):
+	if Input.is_action_just_pressed("toggle_vision_mode"):
+		get_tree().call_group("Interface", "cycle_vision_mode")
