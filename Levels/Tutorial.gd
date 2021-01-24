a05db1ede42f176b1f59a994aec4861d37a6a65e
ColinJWright <colinwright2771@gmail.com>
2021-01-24 10:30:11 +0000

    Lesson 75 complete

---
+++ Levels/Tutorial.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+func _ready():
+	update_pointer_position(0)
+
+func update_pointer_position(objective_number):
+	var pointer = $ObjectivePointer
+	var place = $ObjectivePositions.get_child(objective_number)
+	
+	$Tween.interpolate_property(pointer, "position", pointer.position, place.position, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	$Tween.start()
+	$MessageSound.play()
+
+func _on_MoveObjective_body_entered(body):
+	update_pointer_position(1)
+
+func _on_DoorObjective_body_entered(body):
+	update_pointer_position(2)
+
+func _on_NightVisionObjective_body_entered(body):
+	update_pointer_position(3)
+
+func _on_BriefcaseObjective_body_entered(body):
+	update_pointer_position(4)
