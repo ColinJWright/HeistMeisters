6cfd24c6de287b1aac46e1b5fac5de7071a9b5b4
ColinJWright <colinwright2771@gmail.com>
2021-01-24 13:43:56 +0000

    Changed the text in the computer popup

--- Computer/ComputerPopup.gd
+++ Computer/ComputerPopup.gd
@@ -2,6 +2,6 @@ extends Popup
 
 func set_text(combination):
 	$NinePatchRect/CenterContainer/NinePatchRect/Label.text = (
-			"Your access code is "
-			+ PoolStringArray(combination).join("") +
-			", but I'm not going to keep tellnig you this!")
+			"Hacking......... the door code is "
+			+ PoolStringArray(combination).join("")
+			)
